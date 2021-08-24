24eac494f2afc0dcec95e772b3837f985a12bdee
hotali7 <hotali0106@gmail.com>
2021-08-24 17:58:14 +0900

    fix: updated the layout and loading issue

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,122 +5,12 @@
     class="section page-footer page-footer-alternative bg-gray-darker"
   >
     <div class="shell-wide">
-      <div class="range range-50 range-xs-center range-lg-justify">
-        <div class="cell-sm-6">
-          <h6>Twitter feed</h6>
-          <div class="twitter" data-twitter-username="templatemonster">
-            <div class="twitter-feed-minimal" data-twitter-type="tweet">
-              <div class="unit unit-xs-horizontal">
-                <div class="unit-left">
-                  <span
-                    class="mdi mdi-twitter twitter-icon icon-md-middle"
-                  />
-                </div>
-                <div class="unit-body">
-                  <div
-                    class="twitter-feed-minimal-content"
-                    data-tweet="text"
-                  />
-                  <time
-                    data-date="text"
-                    data-datetime="datetime"
-                    datetime="2017"
-                  />
-                </div>
-              </div>
-            </div>
-            <div class="twitter-feed-minimal" data-twitter-type="tweet">
-              <div class="unit unit-xs-horizontal">
-                <div class="unit-left">
-                  <span
-                    class="mdi mdi-twitter twitter-icon icon-md-middle"
-                  />
-                </div>
-                <div class="unit-body">
-                  <div
-                    class="twitter-feed-minimal-content"
-                    data-tweet="text"
-                  />
-                  <time
-                    data-date="text"
-                    data-datetime="datetime"
-                    datetime="2017"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="cell-sm-6">
-          <h6>Contact US</h6>
-          <ul class="list-xs list-darker">
-            <li class="box-inline">
-              <span
-                class="icon icon-md-smaller icon-primary mdi mdi-map-marker"
-              />
-              <div>
-                <a
-                  href="#"
-                >2130 Fulton Street, Chicago,<br
-                  class="veil reveal-xl-block"
-                >&nbsp; IL 94117-1080 USA</a>
-              </div>
-            </li>
-            <li class="box-inline">
-              <span
-                class="icon icon-md-smaller icon-primary mdi mdi-phone"
-              />
-              <ul class="list-comma">
-                <li><a href="callto:#">1-800-1234-567</a></li>
-                <li><a href="callto:#">1-800-6780-345</a></li>
-              </ul>
-            </li>
-            <li class="box-inline">
-              <span
-                class="icon icon-md-smaller icon-primary mdi mdi-email-open"
-              /><a href="mailto:#">mail@demolink.org</a>
-            </li>
-          </ul>
-          <ul class="page-footer-icon-list social-icons-list">
-            <li>
-              <a
-                class="icon icon-sm-bigger icon-gray-light mdi mdi-facebook"
-                href="#"
-              />
-            </li>
-            <li>
-              <a
-                class="icon icon-sm-bigger icon-gray-light mdi mdi-twitter"
-                href="#"
-              />
-            </li>
-            <li>
-              <a
-                class="icon icon-sm-bigger icon-gray-light mdi mdi-instagram"
-                href="#"
-              />
-            </li>
-            <li>
-              <a
-                class="icon icon-sm-bigger icon-gray-light mdi mdi-google"
-                href="#"
-              />
-            </li>
-            <li>
-              <a
-                class="icon icon-sm-bigger icon-gray-light mdi mdi-linkedin"
-                href="#"
-              />
-            </li>
-          </ul>
-        </div>
-      </div>
       <div class="divider-xl" />
       <div class="range range-xs-middle range-30 range-center">
         <p>
           &#169;&nbsp;<span class="copyright-year" /> All Rights
           Reserved&nbsp;<a href="#">Terms of Use</a>&nbsp;and&nbsp;<a
-            href="privacy-policy.html"
+            href="#"
           >Privacy Policy</a>
         </p>
       </div>
